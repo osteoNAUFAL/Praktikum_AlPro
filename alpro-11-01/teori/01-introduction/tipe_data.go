8cc1a756ee1e22e5fd158a09d2b93150ec00ef6f
osteoNAUFAL <osteonaufal@google.com>
2026-09-21 13:23:18 +0700

    belajar tipe data

---
+++ alpro-11-01/teori/01-introduction/tipe_data.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var umur int8
+	var anakKe int8
+	var saldo int64
+
+	umur = 26
+	anakKe = 1
+	saldo = 1000000000
+
+	fmt.Println("Umur saya adalah", umur)
+	fmt.Println("Saya adalah anak ke", anakKe)
+	fmt.Println("Saldo saya adalah", saldo)
+}
