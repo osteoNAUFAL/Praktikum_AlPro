347e176b030bda217ddf130e455ae493abb92e50
osteoNAUFAL <osteonaufal@google.com>
2026-09-25 01:49:34 +0700

    commit

--- alpro-11-01/praktikum/02-bahasa-pemrograman-go/guided/lingkaran/lingkaran.go
+++ alpro-11-01/praktikum/02-bahasa-pemrograman-go/guided/lingkaran/lingkaran.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
-	
+func main() {
+	// Deklarasi variabel
+	var r, luas float64
+
+	// Assignment nilai pi
+	pi := 3.14
+
+	// Membaca masukan jari-jari lingkaran
+	fmt.Scan(&r)
+
+	// Menghitung luas lingkaran
+	luas = pi * r * r
+
+	// Menampilkan hasil luas lingkaran
+	fmt.Println("Luas lingkaran:", luas)
 }
